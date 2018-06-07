fba97415157563c127e59ae860dba112bd375d83
Your Name <you@example.com>
2018-06-07 21:06:31 +0900

    추가

--- board/models.py
+++ board/models.py
@@ -6,12 +6,11 @@ class CraftPostManager(models.Manager):
 		return super(CraftPostManager, self)
 
 class DjangoBoard(models.Model):
-    id = models.IntegerField(primary_key=True)
-
-    subject = models.CharField(max_length=50, blank=True)
-    name = models.CharField(max_length=50, blank=True)
-    created_date = models.DateField(null=True, blank=True)
-    mail = models.CharField(max_length=50, blank=True)
-    memo = models.CharField(max_length=200, blank=True)
-    hits = models.IntegerField(null=True, blank=True)
-    objects = CraftPostManager()
+	id = models.IntegerField(primary_key=True)
+	subject = models.CharField(max_length=50, blank=True)
+	name = models.CharField(max_length=50, blank=True)
+	created_date = models.DateField(null=True, blank=True)
+	mail = models.CharField(max_length=50, blank=True)
+	memo = models.CharField(max_length=200, blank=True)
+	hits = models.IntegerField(null=True, blank=True)
+	objects = CraftPostManager()
